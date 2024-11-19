ad3801229826ee24106d808d077912243527d620
Marcos Arturo Gomez Lopez <118023196+MarcoSart21@users.noreply.github.com>
2024-11-19 01:23:56 -0600

    FRONT
    Solo se hizo el front falta la parte del back

--- src/controller/page_controller.py
+++ src/controller/page_controller.py
@@ -5,5 +5,16 @@ page = Blueprint('page', __name__)
 
 @page.route('/')
 def index():
-    
     return render_template('index.html')
+
+@page.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+@page.route('/recuperar')
+def recuperar():
+    return render_template('recuperar.html')
+
+@page.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
